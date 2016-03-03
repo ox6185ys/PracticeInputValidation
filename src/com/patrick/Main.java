f69857001dce42ea82cea6f1dd8083a71239aeec
ox6185ys <ox6185ys@go.minneapolis.edu>
2016-03-03 16:06:26 -0600

    New Methods

--- src/com/patrick/Main.java
+++ src/com/patrick/Main.java
@@ -8,15 +8,6 @@ public class Main {
         int cups = 0;
         boolean isNumber;// Validation from Absolute Zero on youtube.
         do {System.out.println("How many cups of coffee did you sell today?");
-            if (scanner.hasNextInt()) {
-                cups = scanner.nextInt();
-                isNumber = true;
-            }else {
-                System.out.println("Please enter an integer.");
-                isNumber = false;
-                scanner.next();
-            }
-        } while (!isNumber);
 
         double price = 0;
         do {System.out.println("What does a cup of coffee cost?");
@@ -29,6 +20,7 @@ public class Main {
                 scanner.next();
             }
         } while (!isNumber);
+
         double coffSales = cupsMoney(cups , price);
 
         System.out.println("How many cups of hot chocolate did you sell today?");
@@ -41,9 +33,9 @@ public class Main {
         System.out.println("How many cups of tea did you sell today?");
         cups = scanner.nextInt();
 
-        System.out.println("What does a cup of tea cost?");
-        price = scanner.nextDouble();
-        double teaSales = cupsMoney(cups , price);
+            System.out.println ("What does a cup of tea cost?");
+            price = scanner.nextDouble ();
+            double teaSales = cupsMoney (cups, price);
 
 //        System.out.println("How many cups of cappuccino did you sell today?");
 //        cups = scanner.nextInt();
@@ -64,14 +56,30 @@ public class Main {
         return drinkMoney;
     }
 
-
     public static double getDrinkInfo(String nameOfDrink){
-        System.out.println("How many cups of " + nameOfDrink + " did you sell today?");
-        int cups = scanner.nextInt();
+        int cups = getValidInteger("How many cups of \" + nameOfDrink + \" did you sell today?");
 
         System.out.println("What does a cup of cappuccino cost?");
         double price = scanner.nextDouble();
         double sales = cupsMoney(cups , price);
         return sales;
+    }
+    public static int getValidInteger(String question) {
+        int cups = 0;
+        boolean isNumber;// Validation from Absolute Zero on youtube.
+        do {
+            System.out.println (question);
+            if (scanner.hasNextInt ()) {
+                cups = scanner.nextInt ();
+                isNumber = true;
+            } else {
+                System.out.println ("Please enter an integer.");
+                isNumber = false;
+                scanner.next ();
+            }
+        } while (!isNumber);
+        return cups;
+    }
+
     }
 }
